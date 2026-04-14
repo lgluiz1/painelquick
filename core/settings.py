811a83f7478ab2cd59f939357982afd8fcdb38b0
lgluiz1 <legalhints@gmail.com>
2026-04-14 14:52:18 -0300

    fix: configure static and media files to serve logos and assets correctly

--- core/settings.py
+++ core/settings.py
@@ -83,7 +83,11 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-from attendance import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls), # Standard admin
     path('', include('attendance.urls')), # Custom app urls
     path('', include('livestream.urls')), # Livestream app urls
 ]
+
+# Habilita o servidor de arquivos estáticos e mídia (Importante para o Logo carregar)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
